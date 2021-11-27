af81600bfbb8cfa5e1be461210ab8a503e14e60e
Jan <jandavearce@jasummeymacsMBP.hitronhub.home>
2021-11-26 19:28:45 -0700

    restart

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.listen(8000, () => {
+app.listen(process.env.PORT || 5000, () => {
  console.log("Server running on port 8000");
 });
 
